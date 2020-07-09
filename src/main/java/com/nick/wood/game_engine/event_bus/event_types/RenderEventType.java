688b82209a3658a0514067da6556f1d87f07f314
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-07-09 13:30:31 +0100

    multi threaded kind of working

---
+++ src/main/java/com/nick/wood/game_engine/event_bus/event_types/RenderEventType.java
@@ -0,0 +1,7 @@
+package com.nick.wood.game_engine.event_bus.event_types;
+
+import com.nick.wood.game_engine.event_bus.interfaces.EventType;
+
+public enum RenderEventType  implements EventType {
+	UPDATE
+}
