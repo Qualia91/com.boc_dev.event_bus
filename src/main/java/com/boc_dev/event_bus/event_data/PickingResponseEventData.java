48a323173294d7fc068b41927ac4c06bfac5e39f
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-11-12 17:44:10 +0000

    added picking and some small fixes to things

--- src/main/java/com/boc_dev/event_bus/event_data/PickingResponseEventData.java
+++ src/main/java/com/boc_dev/event_bus/event_data/PickingResponseEventData.java
@@ -5,12 +5,20 @@ import java.util.UUID;
 public class PickingResponseEventData implements PickingEventData {
 
 	private final UUID uuid;
+	private final String sceneLayerName;
 
-	public PickingResponseEventData(UUID uuid) {
+	public PickingResponseEventData(UUID uuid, String sceneLayerName) {
 		this.uuid = uuid;
+		this.sceneLayerName = sceneLayerName;
 	}
 
 	public UUID getUuid() {
 		return uuid;
 	}
+
+	public String getSceneLayerName() {
+		return sceneLayerName;
+	}
+
+
 }
